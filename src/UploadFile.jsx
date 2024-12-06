354d47b2e3b7f7b77baeaaf3fd557ebe8ec86c1e
izwrt <ishwart466@gmail.com>
2024-12-06 20:34:42 +0530

    uploading progress bar is added

--- src/UploadFile.jsx
+++ src/UploadFile.jsx
@@ -11,14 +11,17 @@ const statuses = {
 const UploadFile = () => {
     const [file, setFile] = useState(null);
     const [status, setStatus] = useState(statuses.IDLE); 
+    const [progressBar, setProgressBar] = useState(null);
 
     function fileHandle(e) {
         e.preventDefault();
         setFile(e.target.files[0])
     }
 
-    async function HandleFileupload() {
+    async function HandleFileupload(e) {
+        e.preventDefault();
         if(!file) return;
+        setProgressBar(0);
         setStatus(statuses.UPLOADING);
 
         const formData = new FormData();
@@ -28,12 +31,18 @@ const UploadFile = () => {
             await axios.post("https://httpbin.org/post", formData, {
                 header: {
                     'Content-Type': 'multipart/form-data'
+                },
+                onUploadProgress: (ProgressEvent) => {
+                    const progress = ProgressEvent.total ? Math.rount((ProgressEvent.loaded * 100) / ProgressEvent.total) : 0;
+                    setProgressBar(progress);
                 }
             });
 
             setStatus(statuses.COMPLETED);
+            setProgressBar(100);
         } catch {
             setStatus(statuses.FAILED)
+            setProgressBar(0);
         };
     }
 
@@ -58,12 +67,12 @@ const UploadFile = () => {
                     {file && status !== 'uploading' && <button
                     onClick={HandleFileupload} className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow w-[200px]">Upload</button>}
 
-                    {file && status !== 'completed' && (
+                    {file && status === 'completed' && (
                         <div className='text-green-600'>Uploaded Sucessfully</div>
                     )}
 
-                    {file && status !== 'failed' &&(
-                        <div>Upload failed :(</div>
+                    {file && status === 'failed' &&(
+                        <div className='text-red-500'>Upload failed :(</div>
                     )}
                 </div>
             )}
